48951a836658bc0ccea3b717d0b24af94ab4baf1
mtuyay-glitch <mtuyay@gmail.com>
2026-01-06 10:51:28 -0800

    Create page.tsx

---
+++ app/import/page.tsx
@@ -0,0 +1,8 @@
+export default function ImportPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>MVVC Import</h1>
+      <p>If you can see this, /import is deployed correctly.</p>
+    </main>
+  );
+}
